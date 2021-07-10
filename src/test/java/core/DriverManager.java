c803f27a31a65d97b77fd07a70fecb34b6920b2f
David Cardenas <davidcarguz@gmail.com>
2021-07-10 17:05:06 -0500

    added changes to make it work with docker compose

--- src/test/java/core/DriverManager.java
+++ src/test/java/core/DriverManager.java
@@ -1,10 +1,13 @@
 package core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import java.net.URL;
 import java.time.Duration;
 
 public class DriverManager {
@@ -12,10 +15,18 @@ public class DriverManager {
     protected static WebDriver driver;
 
     protected void setUpDriver(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
+        try {
+            String nodeUrl = "http://localhost:4444/wd/hub";
+            WebDriverManager.chromedriver().setup();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setPlatform(Platform.LINUX);
+            capabilities.setBrowserName("chrome");
+            driver = new RemoteWebDriver(new URL(nodeUrl), capabilities);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().maximize();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void initializePage(){
